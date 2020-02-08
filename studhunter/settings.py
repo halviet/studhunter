9d1d98e61628a4d00df02dee758841b94e7b6757
Nekmor <smertney@mail.ru>
2020-02-08 15:14:56 +0700

    project

--- studhunter/settings.py
+++ studhunter/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
