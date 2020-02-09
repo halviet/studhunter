cd681f8e87823600242b8a25149f3760020dc4f4
Nekmor <smertney@mail.ru>
2020-02-09 18:34:02 +0700

    ff

--- job/models.py
+++ job/models.py
@@ -65,9 +65,9 @@ class Account(AbstractBaseUser):
 
 class Job(models.Model):
     name = models.CharField(verbose_name='Название', max_length=64, db_index=True)
-    count_people = models.IntegerField(verbose_name='Количество людей', null=False)
-    find = models.IntegerField(verbose_name='Найдено людей', null=False)
-    cost = models.IntegerField(verbose_name='Цена', null=False)
+    count_people = models.IntegerField(verbose_name='Количество людей', blank=True)
+    find = models.IntegerField(verbose_name='Найдено людей', blank=True)
+    cost = models.IntegerField(verbose_name='Цена', blank=True)
     description = models.CharField(verbose_name='Описание', max_length=1024)
     execute_period = models.CharField(verbose_name='Срок сдачи', max_length=20)
     user = models.ForeignKey(Account, verbose_name='Пользователь', on_delete=models.CASCADE)
